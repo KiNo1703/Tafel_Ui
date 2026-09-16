1fdf09d54e49571c5a5d939d11877dc58a4b3fbf
KiNo1703 <kikihell98@gmail.com>
2026-09-16 17:54:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 class Config:
     # ===== Базовые настройки =====
     BASE_URL = "https://manufacture.lar.tech"
-
+    TEST_USERNAME = "qa"
+    TEST_PASSWORD = "qa"
     
     # ===== Таймауты =====
     DEFAULT_TIMEOUT = 15
